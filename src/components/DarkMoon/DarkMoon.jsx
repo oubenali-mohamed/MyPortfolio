5d34c5b6163706b1b12ce24435f31a5d388fdcd7
oubenali mohamed <oubenali15@gmail.com>
2023-09-18 21:42:14 +0200

    modification position button darkmode

--- src/components/DarkMoon/DarkMoon.jsx
+++ src/components/DarkMoon/DarkMoon.jsx
@@ -1,7 +1,7 @@
 import { MoonStar } from 'lucide-react'
 
 const App = () => {
-  return <MoonStar className=" absolute right-40 -mt-4 " size={30} color="#fff" />
+  return <MoonStar className=" absolute right-24 -mt-4 " size={30} color="#fff" />
 }
 
 export default App
