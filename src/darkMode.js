1a5454f2af8c872b221edd7a63f7add684a63bf2
oubenali mohamed <oubenali15@gmail.com>
2023-09-18 10:46:34 +0200

    suppression des commentaire

--- src/darkMode.js
+++ src/darkMode.js
@@ -1,4 +1,3 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (
   localStorage.theme === 'dark' ||
   (!('theme' in localStorage) &&
@@ -9,11 +8,8 @@ if (
   document.documentElement.classList.remove('dark')
 }
 
-// Whenever the user explicitly chooses light mode
 localStorage.theme = 'light'
 
-// Whenever the user explicitly chooses dark mode
 localStorage.theme = 'dark'
 
-// Whenever the user explicitly chooses to respect the OS preference
 localStorage.removeItem('theme')
