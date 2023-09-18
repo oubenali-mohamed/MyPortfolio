9ea80478e20de7e272501b1b921445d91f12cffb
oubenali mohamed <oubenali15@gmail.com>
2023-09-18 10:49:47 +0200

    modification css darkmode

--- src/components/DarkMoon/DarkMoon.jsx
+++ src/components/DarkMoon/DarkMoon.jsx
@@ -1,7 +1,7 @@
 import { MoonStar } from 'lucide-react'
 
 const App = () => {
-  return <MoonStar className=" absolute right-5 " size={30} color="#fff" />
+  return <MoonStar className=" absolute right-10 mt-5 " size={30} color="#fff" />
 }
 
 export default App
